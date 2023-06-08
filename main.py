1d6d20a18e787addd782d14c550d323b74f26dbb
leeyuval <yuval.lee@mail.huji.ac.il>
2023-06-08 13:19:32 +0300

    Pre Process Data Update

--- main.py
+++ main.py
@@ -2,94 +2,98 @@ from typing import List
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 import plotly.graph_objects as go
+
+from multi_classifier import MultiClassifier
 from parser_policy_codes import policy_parser
-from virtualization import show_me, show_me2
+from virtualization import show_me, data_plotting
 
 
 def process_data(data: pd.DataFrame):
-    data = data.fillna(0)
+    # remove dup
     if data.duplicated().any():
         data.drop_duplicates(inplace=False)
-    target = data['cancellation_datetime']
-    # extracting h_booking_id
+    # target label
     h_booking_id = data['h_booking_id']
-
     data.drop('h_booking_id', axis=1)
 
-    data['booking_datetime'] = pd.to_datetime(data['booking_datetime']).dt.dayofyear + (
+    check_in = pd.to_datetime(data['checkin_date']).dt.dayofyear + (
+            pd.to_datetime(data['checkin_date']).dt.year * 365)
+    booking_time = pd.to_datetime(data['booking_datetime']).dt.dayofyear + (
             pd.to_datetime(data['booking_datetime']).dt.year * 365)
-
     # new reservation_wait_time column
-    data['reservation_wait_time'] = pd.to_datetime(data['checkin_date']).dt.dayofyear + (
-            pd.to_datetime(data['checkin_date']).dt.year * 365) - data['booking_datetime']
+    data['time_until_checking_in_days'] = check_in - booking_time
+    data['booking_datetime'] = pd.to_datetime(data['booking_datetime']).dt.month
+    data['checkin_date_month'] = pd.to_datetime(data['checkin_date']).dt.month
+    data['travel_days_long'] = (pd.to_datetime(data['checkout_date']).dt.dayofyear + (
+            pd.to_datetime(data['checkout_date']).dt.year * 365)) - (
+                                       pd.to_datetime(data['checkin_date']).dt.dayofyear + (
+                                       pd.to_datetime(data['checkin_date']).dt.year * 365))
 
-    # converting to days
-
-    data['checkin_date'] = pd.to_datetime(data['checkin_date']).dt.dayofyear
-    data['travel_days_long'] = pd.to_datetime(data['checkout_date']).dt.dayofyear + (
-            pd.to_datetime(data['checkout_date']).dt.year * 365) - pd.to_datetime(data['checkin_date']).dt.dayofyear + (
-                                       pd.to_datetime(data['checkin_date']).dt.year * 365)
-
-    data['cancellation_datetime'] = data['cancellation_datetime'].fillna(0)
-    data['cancellation_datetime'] = pd.to_datetime(data['cancellation_datetime']).dt.dayofyear
-    # to make new ?
-    data['cancellation_datetime_days'] = pd.to_datetime(data['cancellation_datetime']).dt.dayofyear
+    data['is_cancelled'] = (pd.to_datetime(data['cancellation_datetime']).dt.dayofyear).apply(
+        lambda x: 1 if x > 0 else 0)
     encoded_data_accommadation_type_name = pd.get_dummies(data['accommadation_type_name'], prefix='accommadation_type')
     data = pd.concat([data, encoded_data_accommadation_type_name], axis=1)
-    # TODO: not good now
-    # data['charge_option'] = data['charge_option'].replace({'Pay Later': 0, 'Pay Now': 1})
     dummies = pd.get_dummies(data['charge_option'], prefix='charge_option')
-
-    # Concatenate the dummies with the original DataFrame
     data = pd.concat([data, dummies], axis=1)
 
+    # clean data
+    data = data.fillna(0)
+    target = data['is_cancelled']
     # additional features
+    data = data.drop(["cancellation_datetime", "is_cancelled"], axis=1)
     data = data.drop(["h_booking_id", "hotel_id", "checkout_date", "hotel_area_code", "hotel_brand_code",
-                      "origin_country_code", "hotel_country_code",
+                      "origin_country_code", "hotel_country_code", "hotel_city_code",
                       "hotel_chain_code", "hotel_live_date", "h_customer_id", "customer_nationality",
                       "guest_is_not_the_customer", "guest_nationality_country_name", "language",
-                      "original_payment_method", "original_payment_type",
-                      "original_payment_currency", "is_user_logged_in",
-                      "accommadation_type_name", "charge_option"
-                      ],
-                     axis=1)
+                      "original_payment_method", "original_payment_type", "request_nonesmoke",
+                      "original_payment_currency",
+                      "is_first_booking", "no_of_room", "no_of_extra_bed", "no_of_children",
+                      "no_of_adults", "is_user_logged_in", "request_latecheckin",
+                      "request_highfloor", "request_highfloor", "request_twinbeds", "request_airport",
+                      "request_earlycheckin",
+                      "accommadation_type_name", "charge_option", "checkin_date"
+                      ], axis=1)
     # TODO: remove
     data = data.drop(["cancellation_policy_code"], axis=1)
-
     return data, target, h_booking_id
-    data = policy_parser(data)
+    # data = policy_parser(data)
 
 
-path_file = '/Users/alon.frishberg/PycharmProjects/Hackathon/data_src/agoda_cancellation_train.csv'
-# dtype={'column_name': str}
+def corr_func():
+    correlations = np.corrcoef(data, target, rowvar=False)[-1, :-1]
+    feature_names = data.columns.tolist()[:-1]
+    # Print the correlation values
+    for feature_name, correlation in zip(feature_names, correlations):
+        print(f"Correlation between {feature_name} and y: {correlation}")
+
+
+path_file = '/Users/alon.frishberg/PycharmProjects/IML_Hackathon/data_src/agoda_cancellation_train.csv'
 data = pd.read_csv(path_file, encoding='utf-8')
-data, _, _ = process_data(data)
+data, target, h_booking_id = process_data(data)
+data.to_csv('output.csv', index=False)
+# data_plotting(data)
+multi_classifers = MultiClassifier()
 
-# show_me2/(data)
-# show_me(data)
 
-target = data["cancellation_datetime"]
-features = data.drop("cancellation_datetime", axis=1)
-
-classifiers = [
-    RandomForestClassifier(),
-    DecisionTreeClassifier(),
-    LogisticRegression(),
-    SVC(),
-    KNeighborsClassifier(),
-    GaussianNB(),
-    GradientBoostingClassifier()
-]
+# classifiers = [
+#     RandomForestClassifier(),
+#     DecisionTreeClassifier(),
+#     LogisticRegression(),
+#     SVC(),
+#     KNeighborsClassifier(),
+#     GaussianNB(),
+#     GradientBoostingClassifier()
+# ]
 
 
 def understand_features():
@@ -103,27 +107,26 @@ def understand_features():
     # Get feature importances
     feature_importances = clf.feature_importances_
     # Print feature importance scores
-    for feature, importance in zip(features.columns, feature_importances):
+    for feature, importance in zip(data.columns, feature_importances):
         print(f"{feature}: {round(importance, 4)}")
 
 
-def split_and_classify():
-    split_sizes = [0.2, 0.3, 0.4]
-    for classifier in classifiers:
+def split_and_classify(split_sizes=[0.2, 0.3, 0.4]):
+    for classifier in multi_classifers.classifiers:
         print(f"Estimator: {classifier.__class__.__name__}")
         for split_size in split_sizes:
             print(f"Split size: {split_size}")
-
             # Perform train-test split
-            X_train, X_test, y_train, y_test = train_test_split(features, target, train_size=split_size,
+            X_train, X_test, y_train, y_test = train_test_split(data, target, train_size=split_size,
                                                                 test_size=(1 - split_size), random_state=42)
             # Train a Random Forest classifier
             classifier.fit(X_train, y_train)
             # Make predictions on the test set
             y_pred = classifier.predict(X_test)
-
+            y_pred = np.round(y_pred).astype(int)
             # Evaluate the classifier
             accuracy = accuracy_score(y_test, y_pred)
+
             print(f"Accuracy: {accuracy}\n")
 
 
@@ -141,9 +144,9 @@ def union_grid_search():
     # Create a results grid DataFrame
     results_grid = pd.DataFrame(columns=['Classifier', 'Parameters', 'Accuracy'])
     # Iterate over classifiers and parameter grids
-    for classifier, param_grid in zip(classifiers, param_grids):
+    for classifier, param_grid in zip(multi_classifers.classifiers, param_grids):
         # Perform train-test split
-        X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3, random_state=42)
 
         # Perform grid search with cross-validation
         grid_search = GridSearchCV(classifier, param_grid, cv=5)
@@ -165,8 +168,20 @@ def union_grid_search():
     print(results_grid)
 
 
-understand_features()
+# def ensemble(data_test:pd.DataFrame):
+#     ensemble_pred = []
+#     for i in range(len(data_test)):
+#         votes = [pred1[i], pred2[i], pred3[i]]
+#         # Use majority voting for classification
+#         ensemble_pred.append(max(set(votes), key=votes.count))
+#     # Step 6: Evaluate the ensemble model's performance
+#     ensemble_accuracy = accuracy_score(y_test, ensemble_pred)
+#     print("Ensemble Accuracy:", ensemble_accuracy)
+
+
+# ensemble(data_test)
+# understand_features()
 # version 1
-union_grid_search()
+# union_grid_search()
 # # version 2
 split_and_classify()
